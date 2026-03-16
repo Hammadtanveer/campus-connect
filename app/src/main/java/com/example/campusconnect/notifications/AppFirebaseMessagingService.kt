2d9a50e52e13a6abb1fc762998ad1aa6105d4696
Hammad Tanveer <hammadtanveer247@gmail.com>
2026-03-16 17:49:41 +0530

    connect FCM service with NotificationHelper

--- app/src/main/java/com/example/campusconnect/notifications/AppFirebaseMessagingService.kt
+++ app/src/main/java/com/example/campusconnect/notifications/AppFirebaseMessagingService.kt
@@ -13,6 +13,19 @@ class AppFirebaseMessagingService : FirebaseMessagingService() {
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         super.onMessageReceived(remoteMessage)
         Log.d(TAG, "onMessageReceived data=${remoteMessage.data}")
+
+        val title = remoteMessage.data["title"]
+        val body = remoteMessage.data["body"]
+        val type = remoteMessage.data["type"]
+        val targetId = remoteMessage.data["targetId"]
+
+        NotificationHelper.showNotification(
+            context = this,
+            title = title ?: "CampusConnect",
+            body = body ?: "",
+            type = type,
+            targetId = targetId
+        )
     }
 
     override fun onNewToken(token: String) {
@@ -20,4 +33,3 @@ class AppFirebaseMessagingService : FirebaseMessagingService() {
         Log.d(TAG, "onNewToken token=$token")
     }
 }
-
